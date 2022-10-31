4b9190ffec0101de14464f2c7f02188039f50b08
Srinu <srinu.b473@gmail.com>
2022-10-31 22:20:16 +0530

    first commit Micro Service Spring boot architecture

---
+++ src/test/java/com/LearnTechie/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearnTechie.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
